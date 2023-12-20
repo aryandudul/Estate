dba92f4457979ef892133ab2d04339dc9bd9cacd
aryandudul <100128277+aryandudul@users.noreply.github.com>
2023-12-20 23:50:53 +0530

    Cretate Test Api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from  'express' ;
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js'
 dotenv.config() ;
 mongoose.connect(process.env.Mongo).then( ()=>{
     console.log("Connected");
@@ -10,4 +11,8 @@ const app = express() ;
 
 app.listen(3000 , () =>{
     console.log('Server is Running on port number 3000');
-});
+})
+
+app.use('/api/user' , userRouter) ;
+
+;
