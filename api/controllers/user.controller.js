7b8b4a83de569f246a230b0c13e780ae85df1719
aryandudul <100128277+aryandudul@users.noreply.github.com>
2024-01-28 00:29:24 +0530

    CREATE USER UPDATE API

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,46 @@
-export const test  = (req, res ) =>{
- res.json({
-    message: "Api Route Is Working",
- }) ;
-}
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../controllers/utils/error.js';
+import User from '../models/user.model.js';
+
+export const test = (req, res) => {
+    res.json({
+        message: "Api Route Is Working",
+    });
+};
+
+export const updateUser = async (req, res, next) => {
+    try {
+        // Check if the user is trying to update their own account
+        if (req.user.id !== req.params.id) {
+            return next(errorHandler(401, 'You can update your own account!'));
+        }
+
+        // Hash the password if provided
+        if (req.body.password) {
+            req.body.password = bcryptjs.hashSync(req.body.password, 10);
+        }
+
+        // Update user details in the database
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: {
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password,
+                    avatar: req.body.avatar,
+                }
+            },
+            { new: true }
+        );
+
+        // Destructure the updatedUser object
+        const { password, ...rest } = updatedUser._doc;
+
+        // Send the updated user details in the response
+        res.status(200).json(rest);
+    } catch (error) {
+        // Pass the error to the next middleware
+        next(error);
+    }
+};
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser' ;
 
 dotenv.config();
 mongoose.connect(process.env.Mongo).then(() => {
@@ -15,10 +16,12 @@ const app = express();
 
 // Apply express.json() middleware
 app.use(express.json());
+app.use(cookieParser());
 
 // Set up routes
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+
 
 app.use((err , req , res , next) =>{
     const statusCode= err.statusCode || 500;
